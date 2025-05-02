59b8147672a9d1a17e3b48c6d51c166a15217f9a
farmitra01 <farmitra.cloud@gmail.com>
2025-05-02 23:18:40 +0530

    Delete android directory

--- android/app/src/main/kotlin/com/example/farmitra/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.farmitra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/farmitra/farmitra/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.farmitra.farmitra
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
